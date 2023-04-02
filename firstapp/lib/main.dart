b0c5a2b77498ba27a8d3fbac0fd4735a85c4172c
marwan-kotb <marwan.mamdouh.kotb@gmail.com>
2023-04-02 15:21:34 +0200

    business card

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,31 +1,89 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(
     MaterialApp(
-      title: 'Birthday Card',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Birthday Card'),
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.green,
-        ),
-        
-        body: Center(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/image2.jpg'),
+            children: <Widget>[
+              // ignore: prefer_const_constructors
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: const AssetImage('assets/face.jpg'),
+              ),
               const Text(
-                'Happy Birthday !!',
+                'Marwan Mamdouh',
                 style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.blue,
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'BACKEND DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  // ignore: prefer_const_constructors
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+20 1060 505 633',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+
+              Card(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  // ignore: prefer_const_constructors
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'marwan'
+                    '@'
+                    'marwan.io',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
         ),
-
       ),
     ),
   );
